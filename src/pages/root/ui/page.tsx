8a15c09b9b5fd37c08750d18c993aa948ef523d3
And Grigoryan <tednaaa@gmail.com>
2023-05-23 23:54:07 +0400

    chore: remove code line

--- src/pages/root/ui/page.tsx
+++ src/pages/root/ui/page.tsx
@@ -3,8 +3,6 @@ import { Container } from '@/shared/ui';
 import clsx from 'clsx';
 
 export const RootPage: FC = () => {
-  console.log(clsx('foo', true && 'foo-bar'));
-
   return (
     <div>
       <Container>content</Container>
