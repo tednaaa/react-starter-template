e0de124c717b25562cdee48f73ce0c5d749ea3a5
Andranik <tednaaa@gmail.com>
2023-06-25 17:50:30 +0400

    test(e2e): init & config cypress

--- webpack.config.js
+++ webpack.config.js
@@ -6,85 +6,86 @@ const path = require('path');
 
 require('dotenv').config();
 
-const { PORT, NODE_ENV } = process.env;
+const { PORT } = process.env;
 
-const isDevelopment = NODE_ENV === 'development';
+module.exports = (env, argv) => {
+  const isDevelopment = argv?.mode === 'development';
 
-module.exports = {
-  mode: NODE_ENV,
-  entry: path.join(__dirname, 'src', 'app', 'index.tsx'),
-  output: {
-    filename: 'js/[name].[contenthash].js',
-    path: path.join(__dirname, 'dist'),
-    publicPath: '/',
-  },
-  devtool: isDevelopment ? 'inline-source-map' : false,
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
+  return {
+    entry: path.join(__dirname, 'src', 'app', 'index.tsx'),
+    output: {
+      filename: 'js/[name].[contenthash].js',
+      path: path.join(__dirname, 'dist'),
+      publicPath: '/',
     },
-    historyApiFallback: true,
-    hot: true,
-    port: PORT,
-  },
-  optimization: {
-    splitChunks: {
-      chunks: 'all',
+    devtool: isDevelopment ? 'inline-source-map' : false,
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'public'),
+      },
+      historyApiFallback: true,
+      hot: true,
+      port: PORT,
     },
-    minimizer: ['...', new CssMinimizerPlugin()],
-  },
-  performance: {
-    maxEntrypointSize: 512000,
-    maxAssetSize: 512000,
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-    alias: {
-      '@': path.join(__dirname, './src'),
+    optimization: {
+      splitChunks: {
+        chunks: 'all',
+      },
+      minimizer: ['...', new CssMinimizerPlugin()],
     },
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-      favicon: path.join(__dirname, 'public', 'favicon.ico'),
-    }),
-    new MiniCssExtractPlugin({
-      filename: isDevelopment
-        ? 'css/[name].css'
-        : 'css/[name].[contenthash].css',
-    }),
-    new Dotenv({ systemvars: true }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/,
-        exclude: /node_modules/,
-        use: ['ts-loader'],
+    performance: {
+      maxEntrypointSize: 512000,
+      maxAssetSize: 512000,
+    },
+    resolve: {
+      extensions: ['.ts', '.tsx', '.js', '.jsx'],
+      alias: {
+        '@': path.join(__dirname, './src'),
       },
-      {
-        test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          'css-loader',
-          'postcss-loader',
-          'sass-loader',
-        ],
-      },
-      {
-        test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/images/[hash][ext]',
-        },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        type: 'asset/resource',
-        generator: {
-          filename: 'assets/fonts/[hash][ext]',
-        },
-      },
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: path.join(__dirname, 'public', 'index.html'),
+        favicon: path.join(__dirname, 'public', 'favicon.ico'),
+      }),
+      new MiniCssExtractPlugin({
+        filename: isDevelopment
+          ? 'css/[name].css'
+          : 'css/[name].[contenthash].css',
+      }),
+      new Dotenv({ systemvars: true }),
     ],
-  },
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/,
+          exclude: /node_modules/,
+          use: ['ts-loader'],
+        },
+        {
+          test: /\.(css|scss)$/,
+          use: [
+            MiniCssExtractPlugin.loader,
+            'css-loader',
+            'postcss-loader',
+            'sass-loader',
+          ],
+        },
+        {
+          test: /\.(jpg|jpeg|png|gif|mp3|svg)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/images/[hash][ext]',
+          },
+        },
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/,
+          type: 'asset/resource',
+          generator: {
+            filename: 'assets/fonts/[hash][ext]',
+          },
+        },
+      ],
+    },
+  };
 };
